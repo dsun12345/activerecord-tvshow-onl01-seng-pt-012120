4e53f2e073a73151d9ad7f42c6a1cdbe706576e4
Daniel Sun <danielsun12345@gmail.com>
2020-04-02 01:42:32 +0000

    Automatically backed up by Learn

--- app/models/show.rb
+++ app/models/show.rb
@@ -5,7 +5,7 @@ class Show < ActiveRecord::Base
   end 
   
   def Show::most_popular_show
-    Show::highest_rating.first
+    Show.order(rating: :DESC).first
   end 
   
   def Show::lowest_rating
@@ -14,7 +14,10 @@ class Show < ActiveRecord::Base
   
   def Show::least_popular_show
     Show.order(rating: :ASC).first 
-    
+  end 
+  
+  def Show::rating_sum
+    Show.
   end 
   
   
